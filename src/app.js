83efd59e29834dacadbb8bc388134ae9396d64e8
Tabrez Alam <tabrezalam@Tabrezs-MacBook-Pro.local>
2019-05-18 15:18:20 +0530

    setting up heroku

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
+const port = process.env.PORT || 3000
+
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
@@ -93,6 +95,6 @@ app.get('*' , (req , res) => {
         errorMessage : 'Page not found'
     })
 })
-app.listen(3000, () => {
-    console.log('server is running....')
+app.listen(port, () => {
+    console.log('server is running on port ' + port)
 })
